0420c2dfcaf0abf339bcbb062e799cab236a5a70
Sasha Czerniawski <sashacandoit@gmail.com>
2022-10-02 17:03:15 -0400

    notes and comments added

--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -3,17 +3,19 @@ const router = new express.Router()
 const ExpressError = require("../expressError")
 const listItems = require("../fakeDb")
 
-
+/** GET /items - gets all items in list of items */
 router.get("/", function (req, res) {
     return res.json(listItems)
 })
 
+/** POST /items - adds new item to list of items */
 router.post("/", function (req, res) {
     const newItem = { name: req.body.name, price: req.body.price }
     listItems.push(newItem)
     return res.status(201).json({added: newItem})
 })
 
+/** GET /items/[item-name] - gets single item from list by name */
 router.get("/:name", function (req, res) {
     const foundItem = listItems.find(item => item.name === req.params.name)
     if (foundItem === undefined) {
@@ -22,6 +24,7 @@ router.get("/:name", function (req, res) {
     return res.json(foundItem)
 })
 
+/** PATCH /items/[item-name] - updates single item from list by name */
 router.patch("/:name", function (req, res) {
     const foundItem = listItems.find(item => item.name === req.params.name)
     if (foundItem === undefined) {
@@ -32,6 +35,7 @@ router.patch("/:name", function (req, res) {
     return res.status(200).json({ updated: foundItem })
 })
 
+/** DELETE /items/[item-name] - deletes single item from list by name */
 router.delete("/:name", function (req, res) {
     const foundItem = listItems.findIndex(item => item.name === req.params.name)
     if (foundItem === -1) {
